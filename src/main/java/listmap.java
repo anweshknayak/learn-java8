09fec88c01b1c4f681b2011a20042be3f290275a
anweshknayak <anweshknayak@gmail.com>
2019-02-14 18:36:14 +0530

    date example added:

--- src/main/java/listmap.java
+++ src/main/java/listmap.java
@@ -10,18 +10,21 @@ public class listmap {
         List myListOfMaps = new ArrayList();
         Map map1 = new HashMap();
         map1.put("Fname", "Ankur");
-        map1.put("Fna33me", "Andkur");
+       // map1.put("Fname", "Andkur");
 
         Map<String, String> map2 = new HashMap<String, String>();
-        map2.put("Lname", "Singhal");
+        map2.put("Fname", "Singhal");
+        map2.put("Loda", "Singsshal");
 
         myListOfMaps.add(map1);
         myListOfMaps.add(map2);
 
         for (int i = 0 ; i < myListOfMaps.size() ; i++) {
             Map myMap = (Map)myListOfMaps.get(i);
-            myMap.forEach((k,v) -> System.out.println("Key = "
-                    + k + ", Value = " + v));
+            String xar = (String)myMap.get("Loda");
+            System.out.println(xar);
+//            myMap.forEach((k,v) -> System.out.println(
+//                     k + " " + v));
 
         }
     }
